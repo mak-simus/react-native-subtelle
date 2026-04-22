3fd5a1834a254d8258c6a751ac6abf5a85010113
mak-simus <makeshnr97@gmail.com>
2026-04-22 16:42:09 +0530

    Initial commit

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Onboarding = () => {
+  return (
+    <View>
+        <Text>
+            Onboarding
+        </Text>
+    </View>
+  )
+}
+
+export default Onboarding
